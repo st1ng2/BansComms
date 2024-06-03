1022de1f3daea605c1c9a13be46442779252b503
Bahodir <st1ng2@protonmail.ch>
2024-06-03 19:43:16 +0500

    update: Uzbek translations

---
+++ i18n/uz/banscomms.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'server_not_found' => 'Server topilmadi',
+    'params_empty' => 'Baʻzi parametrlar boʻsh',
+    'choose_server' => 'Serverni tanlang',
+    'admin' => [
+        'title' => 'Banlar va ovozsizlar',
+    ],
+    'bans' => [
+        'title' => 'Banlar roʻyxati',
+        'description' => 'Bu sahifada barcha soʻnggi banlar koʻrsatilgan',
+    ],
+    'comms' => [
+        'title' => 'Ovozsizlar roʻyxati',
+        'description' => 'Bu sahifada barcha eng soʻnggi ovozsizlar koʻrsatilgan',
+    ],
+    'table' => [
+        'end_date' => 'Tugash vaqti',
+        'loh' => 'Jazolangan',
+        'reason' => 'Sabab',
+        'admin' => 'Admin',
+        'length' => 'Muddati', 
+        'type' => 'Turi', 
+        'forever' => 'Butun umr',
+        'unbaned' => 'Ban yechilgan',
+        'session' => 'Sessiya',
+        'created' => 'Jazo sanasi'
+    ],
+    'profile' => [
+        'head' => 'Qoidabuzarliklar',
+        'no_info' => 'Qoidabuzarliklar yoʻq 🔥',
+        'steam_not_connected' => 'Bu foydalanuvchi Steam bogʻlamagan 😢'
+    ],
+    'main' => [
+        'admins' => 'Adminlar',
+        'bans' => 'Banlar',
+        'mutes' => 'Ovozsizlar',
+        'gags' => 'Chat bloklar'
+    ]
+];
